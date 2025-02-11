35160ee6b4a4648a9db4195314502ac45339fefb
Esther-Omono <enitanatakere@gmail.com>
2025-02-11 15:11:56 +0100

    Created components for the card

---
+++ src/components/Innercard.jsx
@@ -0,0 +1,3 @@
+export const Innercard = () => {
+  return <div>Innercard</div>;
+};
---
+++ src/components/Outercard.jsx
@@ -0,0 +1,14 @@
+import { Innercard } from './Innercard';
+
+export const Card = () => {
+  return (
+    <div>
+      <div>
+        <h2>Ticket Selection</h2>
+        <p>Step 1/3</p>
+      </div>
+
+      <Innercard />
+    </div>
+  );
+};
