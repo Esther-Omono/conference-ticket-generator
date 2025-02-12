960c38c990f972701778a1b25a34a6a947749c62
Esther-Omono <enitanatakere@gmail.com>
2025-02-12 23:00:44 +0100

    Added mobile responsiveness for ticket selection

--- src/components/Ticketselection.jsx
+++ src/components/Ticketselection.jsx
@@ -6,96 +6,78 @@ const Ticketselection = () => {
   ];
 
   return (
-    <div className='p-6 bg-[#08252b] rounded-2xl border border-[#0e464e] my-7'>
-      <div className='border border-[#0e464e] rounded-2xl p-6 h-[200px] bg-[radial-gradient(103.64%_57.39%_at_14.02%_32.06%,rgba(36,160,181,0.2)_0%,rgba(36,160,181,0)_100%),rgba(10,12,17,0.1)]'>
+    <div className='p-4 sm:p-6 bg-[#08252b] rounded-2xl border border-[#0e464e] my-4 sm:my-7'>
+      <div className='border border-[#0e464e] rounded-2xl p-4 sm:p-6 min-h-[200px] bg-[radial-gradient(103.64%_57.39%_at_14.02%_32.06%,rgba(36,160,181,0.2)_0%,rgba(36,160,181,0)_100%),rgba(10,12,17,0.1)]'>
         {/* TODO: Work on the fonts road rage and roboto */}
         <div className='flex flex-col gap-4'>
-          <h1 className=' text-center text-neutral-50 text-[30px] font-normal font-[Road Rage]'>
+          <h1 className=' text-center text-neutral-50 text-2xl sm:text-[30px] font-normal font-[Road Rage]'>
             Techember Fest &quot;25
           </h1>
-          <p className='text-center text-neutral-50 text-base font-normal leading-normal'>
+          <p className='text-center text-neutral-50 text-sm sm:text-base font-normal leading-normal'>
             Join us for an unforgettable experience at
             <span>
               <br />
             </span>
             HNG12! Secure your spot now.
           </p>
-          <div className='justify-center items-center gap-4 flex text-center'>
-            <p className='text-neutral-50 text-base font-normal leading-normal'>
+          <div className='flex flex-col sm:flex-row justify-center items-center gap-2 sm:gap-4 text-center'>
+            <p className='text-neutral-50 text-sm sm:text-base font-normal leading-normal'>
               📍 Lagos
             </p>
-            <p className='text-neutral-50 text-base font-normal leading-normal'>
+            <p className='hidden sm:block text-neutral-50 text-base font-normal leading-normal'>
               | |
             </p>
-            <p className='text-neutral-50 text-base font-normal leading-normal'>
+            <p className='text-neutral-50 text-sm sm:text-base font-normal leading-normal'>
               March 15, 2025 | 7:00 PM
             </p>
           </div>
         </div>
       </div>
 
-      <hr className='my-6 text-[#07373f] border-2 border-[#07373f]' />
+      <hr className='my-4 sm:my-6 text-[#07373f] border-2 border-[#07373f]' />
 
-      <div className='h-[142px] w-full p-4 bg-[#042127] rounded-2xl border border-[#07363e] flex-row justify-center items-center gap-4 inline-flex'>
+      <div className='w-full p-4 bg-[#042127] rounded-2xl border border-[#07363e] flex flex-col sm:flex-row justify-center items-center gap-4'>
         {ticketOption.map(({ ticket, access, number }) => {
           return (
             <button
               key={ticket}
-              className='w-[140px] h-[110px] flex flex-col gap-4 p-3 rounded-xl border-2 border-[#197686] text-left'
+              className='w-full sm:w-[140px] p-3 rounded-xl border-2 border-[#197686] text-left flex flex-row sm:flex-col justify-between sm:justify-start items-start gap-4 '
             >
-              <p className='text-white text-xl font-semibold'>{ticket}</p>
+              <p className='text-white text-lg sm:text-xl font-semibold'>
+                {ticket}
+              </p>
 
-              <div className='flex-col justify-center items-start flex '>
-                <p className='text-neutral-50 text-sm font-normal uppercase'>
+              <div className='flex flex-col justify-center items-start'>
+                <p className='text-neutral-50 text-xs sm:text-sm font-normal uppercase'>
                   {access}
                 </p>
-                <p className='text-[#d9d9d9] text-sm font-normal'>{number}</p>
+                <p className='text-[#d9d9d9] text-xs sm:text-sm font-normal'>
+                  {number}
+                </p>
               </div>
             </button>
           );
         })}
       </div>
       {/* TODO: text color for options */}
-      <div className='h-28 flex-col justify-start items-start gap-2 flex w-full mt-5'>
-        <label className="text-neutral-50 text-base font-normal font-['Roboto'] leading-normal">
+      <div className='mt-5 flex flex-col gap-2 w-full'>
+        <label className="text-neutral-50 text-sm sm:text-base font-normal font-['Roboto'] leading-normal">
           Number of Tickets:
         </label>
-        <select className='w-full p-3 rounded-xl border text-white border-[#07363e] justify-center items-center inline-flex'>
-          <option className="text-white text-base font-normal font-['Roboto'] leading-normal">
-            1
-          </option>
-          <option className="text-white text-base font-normal font-['Roboto'] leading-normal">
-            2
-          </option>
-          <option className="text-white text-base font-normal font-['Roboto'] leading-normal">
-            3
-          </option>
-          <option className="text-white text-base font-normal font-['Roboto'] leading-normal">
-            4
-          </option>
-          <option className="text-white text-base font-normal font-['Roboto'] leading-normal">
-            5
-          </option>
-          <option className="text-white text-base font-normal font-['Roboto'] leading-normal">
-            6
-          </option>
-          <option className="text-white text-base font-normal font-['Roboto'] leading-normal">
-            7
-          </option>
-          <option className="text-white text-base font-normal font-['Roboto'] leading-normal">
-            8
-          </option>
-          <option className="text-white text-base font-normal font-['Roboto'] leading-normal">
-            9
-          </option>
+        <select className='w-full p-3 rounded-xl border text-white border-[#07363e] bg-transparent text-sm sm:text-base'>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => (
+            <option key={num} value={num} className='text-white bg-[#042127]'>
+              {num}
+            </option>
+          ))}
         </select>
       </div>
 
-      <div className='h-12 justify-end items-end gap-6 flex w-full'>
-        <button className='rounded-lg border border-[#23a0b5] py-3 text-[#23a0b5] w-1/2 text-base font-normal font-[JejuMyeongjo] leading-normal'>
+      <div className='mt-6 flex gap-4 sm:gap-6 w-full'>
+        <button className='rounded-lg border border-[#23a0b5] py-2 sm:py-3 text-[#23a0b5] w-1/2 text-sm sm:text-base font-normal font-[JejuMyeongjo] leading-normal'>
           Cancel
         </button>
-        <button className='bg-[#23a0b5] rounded-lg text-white py-3 w-1/2 text-base font-normal font-[JejuMyeongjo] leading-normal'>
+        <button className='bg-[#23a0b5] rounded-lg text-white py-2 sm:py-3 w-1/2 text-sm sm:text-base font-normal font-[JejuMyeongjo] leading-normal'>
           Next
         </button>
       </div>
