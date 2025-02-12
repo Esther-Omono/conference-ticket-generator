0cf4a637ca00b04d2b8cc306a55ecd04757293d3
Esther-Omono <enitanatakere@gmail.com>
2025-02-12 13:13:43 +0100

    Step 2 desktop done

--- src/components/Attendeedetails.jsx
+++ src/components/Attendeedetails.jsx
@@ -2,7 +2,7 @@ import { CloudDownload } from 'lucide-react';
 
 export const Attendeedetails = () => {
   return (
-    <div className='p-6 bg-[#08252b] rounded-2xl border border-[#0e464e] mt-5'>
+    <div className='p-6 bg-[#08252b] rounded-2xl border border-[#0e464e] my-7'>
       <div className='w-[495px] h-[250px] px-5 pt-4 pb-12 bg-[#042127] rounded-2xl border border-[#07363e]  flex-col justify-start items-start gap-8 inline-flex relative z-10'>
         <h2 className='text-center text-neutral-50 text-sm font-normal leading-normal'>
           Upload Profile Photo
@@ -16,6 +16,55 @@ export const Attendeedetails = () => {
           </div>
         </div>
       </div>
+
+      <hr className='my-4 text-[#07373f] border-2 border-[#07373f]' />
+
+      {/* TODO: Fix the input focus and placeholder padding? Import Roboto font */}
+      <form className='space-y-4'>
+        <div className=' w-full flex-col justify-start items-start gap-2 flex'>
+          <label className=' text-neutral-50 text-sm font-normal leading-normal'>
+            Enter your name
+          </label>
+          <input
+            className='border border-[#07373f] text-white text-base font-normal px-2.5 py-1.5 w-full rounded-xl'
+            required
+          />
+        </div>
+
+        <div className='self-stretch flex-col justify-start items-start gap-2 flex'>
+          <label className='self-stretch text-neutral-50 text-sm font-normal leading-normal'>
+            Enter your email *
+          </label>
+          <input
+            className='border border-[#07373f] text-white text-base font-normal px-2.5 py-1.5 w-full rounded-xl'
+            required
+            placeholder='hello@avioflagos.io'
+          />
+        </div>
+
+        <div className='self-stretch h-[159px] flex-col justify-start items-start gap-2 flex'>
+          <label className='self-stretch text-neutral-50 text-sm leading-normal'>
+            Special request?
+          </label>
+          <textarea className='self-stretch h-[127px] p-3 rounded-xl border border-[#07363e] justify-start items-start gap-2 inline-flex'></textarea>
+        </div>
+
+        <div className='h-12 justify-end items-end gap-6 flex'>
+          <div className='grow shrink basis-0 h-12 justify-start items-start gap-4 flex'>
+            <div className='grow shrink basis-0 h-12 px-6 py-3 rounded-lg border border-[#23a0b5] justify-center items-center gap-2 flex overflow-hidden'>
+              <button className='text-[#23a0b5] text-base font-normal font-[JejuMyeongjo] leading-normal'>
+                Back
+              </button>
+            </div>
+          </div>
+
+          <div className='grow shrink basis-0 h-12 px-6 py-3 bg-[#23a0b5] rounded-lg justify-center items-center gap-2 flex overflow-hidden'>
+            <button className='text-white text-base font-normal font-[JejuMyeongjo] leading-normal'>
+              Get My Free Ticket
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
